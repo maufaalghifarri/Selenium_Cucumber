39ac02cf272746a1c7cf875768d547a813389b1f
maufaalghifarri <149165218+maufaalghifarri@users.noreply.github.com>
2023-11-23 12:13:53 +0700

    Update CucumberTest.java

--- src/test/java/Aufa/CucumberTest.java
+++ src/test/java/Aufa/CucumberTest.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         glue = {"Aufa"},
         features = {"src/test/resources"},
-        plugin = {"pretty"}
+        plugin = {"pretty", "html:reports/cucumber.html", "json:reports/cucumber.json"}
 )
 public class CucumberTest {
 }
